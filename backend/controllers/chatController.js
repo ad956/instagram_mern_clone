b624573247fd44f712ec1bfcfb8e7a9a17abf8f2
Anand Suthar <anandsuthar956@gmail.com>
2023-12-31 13:35:13 +0530

    chatcontroller updated

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -1,45 +1,41 @@
 const catchAsync = require("../middlewares/catchAsync");
 const Chat = require("../models/chatModel");
 
-// Create New Chat
 exports.newChat = catchAsync(async (req, res, next) => {
+  const chatExists = await Chat.findOne({
+    users: {
+      $all: [req.user._id, req.body.receiverId],
+    },
+  });
 
-    const chatExists = await Chat.findOne({
-        users: {
-            $all: [req.user._id, req.body.receiverId]
-        }
+  if (chatExists) {
+    return res.status(200).json({
+      success: true,
+      newChat: chatExists,
     });
+  }
 
-    if (chatExists) {
-        return res.status(200).json({
-            success: true,
-            newChat: chatExists
-        });
-    }
+  const newChat = await Chat.create({
+    users: [req.user._id, req.body.receiverId],
+  });
 
-    const newChat = await Chat.create({
-        users: [req.user._id, req.body.receiverId],
-    });
-
-    res.status(200).json({
-        success: true,
-        newChat
-    });
+  res.status(200).json({
+    success: true,
+    newChat,
+  });
 });
 
-// Get All Chats
 exports.getChats = catchAsync(async (req, res, next) => {
+  const chats = await Chat.find({
+    users: {
+      $in: [req.user._id],
+    },
+  })
+    .sort({ updatedAt: -1 })
+    .populate("users latestMessage");
 
-    const chats = await Chat.find(
-        {
-            users: {
-                $in: [req.user._id]
-            }
-        }
-    ).sort({ updatedAt: -1 }).populate("users latestMessage");
-
-    res.status(200).json({
-        success: true,
-        chats
-    });
+  res.status(200).json({
+    success: true,
+    chats,
+  });
 });

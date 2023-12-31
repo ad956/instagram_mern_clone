9910aec80851febbc5c4002f12556b3bb52ff369
Anand Suthar <anandsuthar956@gmail.com>
2023-12-31 13:30:22 +0530

    initial commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDatabase = () => {
-    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  mongoose
+    .connect(process.env.DB_URL)
     .then(() => {
-        console.log("Mongoose Connected");
-    }).catch((error) => {
-        console.log(error);
+      console.log("Mongoose Connected");
+    })
+    .catch((error) => {
+      console.log(error);
     });
-}
+};
 
 module.exports = connectDatabase;

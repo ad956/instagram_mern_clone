e11a1669535f524469e7fd21c2bd27812fa0b804
Anand Suthar <anandsuthar956@gmail.com>
2023-12-31 13:01:01 +0530

    intial

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,57 @@
+export const SOCKET_ENDPOINT = "http://localhost:4000";
+// export const SOCKET_ENDPOINT = "https://instagrammern.herokuapp.com";
+
+export const stories = [
+  {
+    title: "JavaScript",
+    image: "javascript",
+  },
+  {
+    title: "Node.js",
+    image: "nodejs",
+  },
+  {
+    title: "Express.js",
+    image: "expressjs",
+  },
+  {
+    title: "MongoDB",
+    image: "mongodb",
+  },
+  {
+    title: "React.js",
+    image: "reactjs",
+  },
+  {
+    title: "Socket.io",
+    image: "socketio",
+  },
+  {
+    title: "TailwindCSS",
+    image: "tailwind",
+  },
+  {
+    title: "MaterialUI",
+    image: "mui",
+  },
+  {
+    title: "Redux",
+    image: "redux",
+  },
+  {
+    title: "Multer",
+    image: "multer",
+  },
+  {
+    title: "Sendgrid",
+    image: "sendgrid",
+  },
+  {
+    title: "Axios",
+    image: "axios",
+  },
+  {
+    title: "Toastify",
+    image: "toastify",
+  },
+];
